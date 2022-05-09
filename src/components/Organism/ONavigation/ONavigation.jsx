439f2c32339de05ba047106ea726ad27dc17c187
firatmelih <melih.firat@ogr.sakarya.edu.tr>
2022-05-09 15:03:03 +0200

    nav bem update

--- src/components/Organism/ONavigation/ONavigation.jsx
+++ src/components/Organism/ONavigation/ONavigation.jsx
@@ -7,10 +7,11 @@ function ONavigation({color}){
     let className = `o-navigation -${color?color:'primary'}`
     return(
         <div className={className}>
-            <div className="logo">
+            <div className="o-navigation__logo">
                 <AText level={'large'} color={'red'} bold> f </AText><AText
                 level={'large'}  color={'blue'} bold> m </AText></div>
             <div>
+                {/*//here will be a component by itself*/}
                 <div className={'m-search'}>
                     <AInput placeHolder={'Search'} type={'name'} value={''}/>
                     <SearchRounded fontSize ={'large'}/>
